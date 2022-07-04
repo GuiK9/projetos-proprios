73281aa3fc12303a37b5e248b9f58fbd41d1b668
Guilherme Sousa <guilherme.tec1107@gmail.com>
2022-07-04 14:21:45 -0300

    test docker run

--- API-CRUD_MongoDB_Node.JS/api.js
+++ API-CRUD_MongoDB_Node.JS/api.js
@@ -19,15 +19,15 @@ app.use("/", routes)
 
 
 
-//    memory usage log
+/*  memory usage log
 
-setInterval(() => {
+ setInterval(() => {
     const used = process.memoryUsage().heapUsed / 1024 / 1024;
     console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`)
-    
-}, 100) 
+
+}, 100) */
 
 
-module.exports = app.listen(process.env.PORT, () => {
+module.exports = app.listen(process.env.PORT, process.env.HOST, () => {
     console.log("running")
 })
